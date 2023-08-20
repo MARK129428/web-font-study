8b87a06f629d739853ddb7887ca521b112462eef
何鸿凯 <1544832671@qq.com>
2023-08-20 10:30:27 +0800

    test

---
+++ webpack-demo1/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html')
+        })
+    ],
+    output: {
+    }
+}
